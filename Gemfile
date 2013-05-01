1a88b020dfa97d26ff9573cdb45475a6b4537e72
hmind <hmind2005@gmail.com>
2013-05-01 15:47:03 -0400

    Add ruby-units gem and add area code

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'ruby-units'
+
 # Specify your gem's dependencies in metric_conversions.gemspec
 gemspec
--- lib/metric_conversions/areas.rb
+++ lib/metric_conversions/areas.rb
@@ -1,3 +1,30 @@
+require 'rubygems'
+require 'ruby-units'
+
 module MetricConversions
-  # Your code goes here...
+  class Area
+    def self.method_missing(name, *args)
+      if name =~ /convert_\w+_to_\w+/
+        unit1, unit2 = name.to_s.scan(/convert_(\w+)_to_(\w+)/).first[0..1]
+        
+        convert(args[0], unit1, unit2)
+      else
+        super
+      end
+    end
+
+    def self.convert(num1, unit1, unit2)
+      if Unit(unit1) && Unit(unit2) && Unit(unit1).compatible?(Unit(unit2)) && num1.to_i
+        Unit(num1.to_s + ' ' + unit1).convert_to(unit2).to_int
+      else
+        throw InvalidUnitException
+      end
+    end
+  end
+
+  class InvalidUnitException < Exception
+    def message
+      "Unit is invalid"
+    end
+  end
 end
